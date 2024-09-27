10ecc46f8e80922efd85f734f7465e5d39f3dfd2
JooHelder <jhoalvesb@gmail.com>
2024-09-27 01:46:22 -0300

    Lista de exercícios pt1

--- Ex02/src/Main.java
+++ Ex02/src/Main.java
@@ -1,6 +1,91 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /*
+        // Questão 1
+        int soma = 0;
+        int[] A = {1, 0 ,5 , -2, -5, 7};
+        soma = A[0] + A[1] + A[5];
+        System.out.println(soma);
+        A[4]= 100;
+        for (int i = 0; i < A.length; i++){
+            System.out.println(A[i]);
+        }
+        // Questão 2
+        Scanner sc = new Scanner(System.in);
+        int [] vetor = new int[5];
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Escreva um número: ");
+            vetor[i] = sc.nextInt();
+        }
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+        // Questão
+        Scanner sc = new Scanner(System.in);
+        double[] vetor = new double[10];
+        double[] vetorQuad = new double[10];
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = sc.nextDouble();
+            vetorQuad[i] = vetor[i] * vetor[i];
+        }
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < vetorQuad.length; i++) {
+            System.out.print(vetorQuad[i] + " ");
+        }
+        // Questão 4
+        Scanner sc = new Scanner(System.in);
+        int[] vetor = new int[8];
+        for (int i = 0; i < vetor.length; i++){
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = sc.nextInt();
+        }
+        System.out.print("Escreva uma posição do vetor: ");
+        int X = sc.nextInt();
+        System.out.print("Escreva outra posição: ");
+        int Y = sc.nextInt();
+        int soma = 0;
+        soma = vetor[X] + vetor[Y];
+        System.out.println("O resultado da soma dos números na posição indicada é: " + soma);
 
-        System.out.println("Hello world!");
+        // Questão 5
+        Scanner sc = new Scanner(System.in);
+        int[] vetor = new int[10];
+        int contadorPar = 0;
+        for (int i = 0; i < vetor.length; i++){
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = sc.nextInt();
+            if (vetor[i] % 2 == 0){
+                contadorPar++;
+            }
+        }
+        System.out.println("Seu vetor tem "+ contadorPar + " números pares");
+
+        // Questão 6
+        Scanner sc = new Scanner(System.in);
+        int[] vetor = new int[10];
+        for (int i = 0; i < vetor.length; i++){
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = sc.nextInt();
+        }
+        int num = vetor[0];
+        for (int i = 0; i < vetor.length; i++){
+            if (vetor[i] > num){
+                num = vetor[i];
+            }
+        }
+        System.out.println("O maior valor é: " + num);
+        for (int i = 0; i < vetor.length; i++){
+            if (vetor[i] < num){
+                num = vetor[i];
+            }
+        }
+        System.out.println("O menor número é: " + num);*/
+
+        
     }
 }
